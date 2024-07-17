4f7eb8e0bb8cca10b7877d7f3e5cea28f7824548
Thullyoo <thullyofilho@gmail.com>
2024-07-17 13:17:32 -0300

    Initial commit

---
+++ src/main/java/com/thullyoo/gerenciador_vendas/GerenciadorVendasApplication.java
@@ -0,0 +1,13 @@
+package com.thullyoo.gerenciador_vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorVendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thullyoo/gerenciador_vendas/GerenciadorVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thullyoo.gerenciador_vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
