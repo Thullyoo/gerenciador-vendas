7379c829734699bd48ed294be7f02a237d79b5d3
Thullyoo <thullyofilho@gmail.com>
2024-07-18 21:17:33 -0300

    feat: adicionado o controller para variação e um post para o service de registro de variação

---
+++ src/main/java/com/thullyoo/gerenciador_vendas/controllers/VariacaoController.java
@@ -0,0 +1,22 @@
+package com.thullyoo.gerenciador_vendas.controllers;
+
+import com.thullyoo.gerenciador_vendas.dtos.VariacaoRequest;
+import com.thullyoo.gerenciador_vendas.dtos.VariacaoResponse;
+import com.thullyoo.gerenciador_vendas.services.VariacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/variacao")
+public class VariacaoController {
+
+    @Autowired
+    private VariacaoService variacaoService;
+
+    @PostMapping()
+    public ResponseEntity<VariacaoResponse> registrarVariacao(@RequestBody VariacaoRequest variacaoRequest){
+        return ResponseEntity.status(HttpStatus.CREATED).body(variacaoService.registrarVariacao(variacaoRequest));
+    }
+}
