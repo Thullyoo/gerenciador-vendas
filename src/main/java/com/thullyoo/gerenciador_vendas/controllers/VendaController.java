a829e945bb98035cb43c0288c65dea60f3adc657
Thullyoo <thullyofilho@gmail.com>
2024-07-19 12:37:48 -0300

    feat: adição de controller Venda para registro de venda

---
+++ src/main/java/com/thullyoo/gerenciador_vendas/controllers/VendaController.java
@@ -0,0 +1,26 @@
+package com.thullyoo.gerenciador_vendas.controllers;
+
+import com.thullyoo.gerenciador_vendas.dtos.VendaRequest;
+import com.thullyoo.gerenciador_vendas.dtos.VendaResponse;
+import com.thullyoo.gerenciador_vendas.services.VendaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/vendas")
+public class VendaController {
+
+    @Autowired
+    private VendaService vendaService;
+
+    @PostMapping
+    public ResponseEntity<VendaResponse> registrarVenda(@RequestBody VendaRequest vendaRequest){
+        return ResponseEntity.status(HttpStatus.CREATED).body(vendaService.registrarVenda(vendaRequest));
+    }
+
+}
