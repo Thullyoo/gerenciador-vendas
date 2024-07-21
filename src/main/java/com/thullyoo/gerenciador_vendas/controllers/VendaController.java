134cf152da5fc7e5652f84fa8aaba360a1657636
Thullyoo <thullyofilho@gmail.com>
2024-07-21 20:29:51 -0300

    fix: adicao de metodo Get para o resgate das vendas registradas

--- src/main/java/com/thullyoo/gerenciador_vendas/controllers/VendaController.java
+++ src/main/java/com/thullyoo/gerenciador_vendas/controllers/VendaController.java
@@ -6,10 +6,9 @@ import com.thullyoo.gerenciador_vendas.services.VendaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/vendas")
@@ -21,6 +20,11 @@ public class VendaController {
     @PostMapping
     public ResponseEntity<VendaResponse> registrarVenda(@RequestBody VendaRequest vendaRequest){
         return ResponseEntity.status(HttpStatus.CREATED).body(vendaService.registrarVenda(vendaRequest));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<VendaResponse>> resgatarTodasAsVendas(){
+        return ResponseEntity.status(HttpStatus.FOUND).body(vendaService.resgatarTodasAsVendas());
     }
 
 }
