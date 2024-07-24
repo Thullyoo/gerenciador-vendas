9c0b17df66ec5d5647b0c92fed40b7a8e0eabbcb
Thullyoo <thullyofilho@gmail.com>
2024-07-24 12:37:44 -0300

    feat: criação de metodo para query por data

--- src/main/java/com/thullyoo/gerenciador_vendas/repositories/VendaRepository.java
+++ src/main/java/com/thullyoo/gerenciador_vendas/repositories/VendaRepository.java
@@ -1,10 +1,19 @@
 package com.thullyoo.gerenciador_vendas.repositories;
 
-import com.thullyoo.gerenciador_vendas.entities.Venda;
+import com.thullyoo.gerenciador_vendas.entities.venda_models.Venda;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
 
 @Repository
 public interface VendaRepository extends JpaRepository<Venda, Long> {
+
+    @Query(value = "SELECT * FROM tb_vendas WHERE DATE(horario_de_venda) = DATE(:dataEspecifica)", nativeQuery = true)
+    List<Venda> findBySpecificDate(@Param("dataEspecifica") LocalDate dataEspecifica);
 
 }
